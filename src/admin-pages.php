96905a0ca1630101e6b76c7bc147bb732603a01d
Gabo Mota Chong <gabomota.me@gmail.com>
2025-07-31 20:28:02 -0300

    improve graphics rendering

--- src/admin-pages.php
+++ src/admin-pages.php
@@ -50,14 +50,49 @@ function dfw_dashor_woo_visualizer_page() {
             max-width: 100%;
             height: auto;
             text-align: center;
-            padding: 50px;
-            font-size: 60px;
-            font-weight: bolder;        
+            padding: 40px;
+            font-size: 50px;
+            font-weight: bolder;
+            color: #4E79A7;     
+        }
+
+        .styled-list {
+            list-style-type: none;
+            padding: 15px;
+        }
+
+        .styled-list-success li {
+            background-color: #d9ead3;  
+            color: darkgreen;
+            font-size: 18px;
+            margin-top: 4px;            
+            margin-bottom: 4px;
+            text-align: center;
+            padding: 10px;            
+        }
+        
+        .styled-list-danger li {
+            background-color: #f4cccc;
+            color: darkred;
+            font-size: 18px;      
+            margin-top: 4px;            
+            margin-bottom: 4px;
+            text-align: center;
+            padding: 10px;            
+        }
+        
+        .styled-list-info li {
+            background-color: #d0e0e3;            
+            color: darkblue;
+            font-size: 18px;            
+            margin-top: 4px;            
+            margin-bottom: 4px;
+            text-align: center;
+            padding: 10px;            
         }        
     </style>
     <?php
-
-    echo '<div class="wrap"><h1>Dashor Woo Visualizer - Charts</h1>';
+    echo '<div class="wrap"><h1>Dashor for Woocommerce - Charts</h1>';
 
         echo '<div class="woo-dashboard-label-grid">';
             echo '<div class="label-card"><h3>Total clientes</h3><div id="totalCustomersLabel"></div></div>';
@@ -72,16 +107,13 @@ function dfw_dashor_woo_visualizer_page() {
             echo '<div class="chart-card"><h3>Total de pedidos por status</h3><canvas id="totalOrdersByStatusChart"></canvas></div>';
             echo '<div class="chart-card"><h3>Total de ganhos por status de pedidos</h3><canvas id="totalRevenueByStatusChart"></canvas></div>';
 
-            echo '<div class="chart-card"><h3>Produtos mais vendidos</h3><canvas id="topMostOrderedProductsChart"></canvas></div>';
-            echo '<div class="chart-card"><h3>Produtos menos vendidos</h3><canvas id="topLeastOrderedProductsChart"></canvas></div>';
-            
-
             echo '<div class="chart-card"><h3>Produtos com baixo stock</h3><canvas id="productsLowStockChart"></canvas></div>';
             echo '<div class="chart-card"><h3>Total produtos por categoria</h3><canvas id="productsByCategoryChart"></canvas></div>';
             
-            /*
-            echo '<div class="chart-card"><h3>Clientes con mais pedidos</h3><canvas id="topCustomersChart"></canvas></div>';
-            echo '<div class="chart-card"><h3>Clientes</h3><canvas id="customersChart"></canvas></div>';*/
+            echo '<div class="chart-card"><h3>Produtos mais vendidos</h3><ul id="topMostOrderedProductsList" class="styled-list-success"></ul></div>';
+            echo '<div class="chart-card"><h3>Produtos menos vendidos</h3><ul id="topLeastOrderedProductsList" class="styled-list-danger"></ul></div>';
+
+            echo '<div class="chart-card"><h3>Clientes com mais pedidos</h3><ul id="topCustomersOrdersList" class="styled-list-info"></ul></div>'; 
     
         echo '</div>';
 
